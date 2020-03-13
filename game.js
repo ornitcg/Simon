7b9f15c66a69b0295365ab82d150df88731bae2a
ornitcohen <tink.ornitc@gmail.com>
2020-03-13 16:04:23 +0200

    comments added

--- game.js
+++ game.js
@@ -1,50 +1,28 @@
-// alert("hello");
+//THE SIMON GAME
+//by Ornit Cohn Gindi
+//guided by 'The Complete 2020 Web Development Bootcamp' on Udemy
+
+//in this game a sequence of flickering colored squars is given and the player should
+//follow it without mistakes.
+//once a mistake is done, the game is over.
 var buttonColors = ["red", "green", "yellow", "blue"];
-var gamePattern = [];
-var userClickedPattern = [];
+var gamePattern = []; //will contain the array of colors to follow
+var userClickedPattern = []; //will contain the player's answer
 var started = false;
 var level = -1;
 
-//this part is listening to the keytboard
+//this part is listening to the keytboard, to initiate a new game
 $(document).on("keypress", function (event) {
-
     if (!started) {
         level = -1;
         started = true;
         nextSequence(); // 
     }
-
 });
 
-function nextSequence() {
-    level++;
 
-    $("#level-title").text("Level " + level);
 
-    setTimeout(function () {
-        var randomNumber = Math.floor(Math.random() * 10 + 1) % 4; //randomizes a number
-        var randomChosenColor = buttonColors[randomNumber]; //translates a number to a color
-        gamePattern.push(randomChosenColor); //adds color to array
-        console.log(gamePattern);
-    }, 0);
 
-    var i = 0;
-    while (i <= level) {
-        console.log(i + " level " + level);
-        fadeSqr(i);
-        i++;
-    }
-    //display the new sequence
-
-    console.log("finished while"); ///debug
-
-}
-
-function fadeSqr(i) {
-    setTimeout(function () {
-        $("#" + gamePattern[i]).fadeIn(200).fadeOut(200).fadeIn(200);
-    }, 500 * i);
-}
 
 //this part is listenng to the  mouse clicks
 $(".btn").on("click", function () {
@@ -78,7 +56,31 @@ $(".btn").on("click", function () {
     }
 });
 /////////////////////// functions //////////////////////
+function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level); // updates the title to the relevant level
 
+    var randomNumber = Math.floor(Math.random() * 10 + 1) % 4; //randomizes a number
+    var randomChosenColor = buttonColors[randomNumber]; //translates a number to a color
+    gamePattern.push(randomChosenColor); //adds color to array
+
+    //the following loop will display the new sequence
+    var i = 0;
+    while (i <= level) {
+        console.log(i + " level " + level);
+        fadeSqr(i);
+        i++;
+    }
+}
+
+//this method flickers a square that is of a color that is on the i'th 
+//place in the gamePattern array, in delay so thhe whole sequence is 
+//possible to follow
+function fadeSqr(i) {
+    setTimeout(function () {
+        $("#" + gamePattern[i]).fadeIn(200).fadeOut(200).fadeIn(200);
+    }, 500 * i);
+}
 
 //this method plays a sound according to a name of color
 function playSoundName(colorName) {
